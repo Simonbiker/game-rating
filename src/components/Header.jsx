026acf08213fd3ef7bfcbebdb2fd00f60ae3b333
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-06-29 15:42:19 +0100

    Changing the styling of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-      <header className='container text-center bg-darkBlue mx-auto'>
+      <header className='container text-center bg-darkBlue mx-auto max-w-7xl md:p-10'>
           <h1 className="text-3xl text-white px-6 py-12">
               Game Rating
           </h1>

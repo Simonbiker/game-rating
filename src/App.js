fbabbc8945c7e97c57721db849c1e55c3150db28
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-06-30 15:11:32 +0100

    Changes to package file so I can run server and react at the same time.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 
+// Context
+import { GameRatingProvider } from './context/GameRatingContext'
+
 // Components
 import Header from './components/Header';
+import GameRatingForm from './components/GameRatingForm';
 import GameRatingList from './components/GameRatingList';
 
 function App() {
   return (
-    <>
+    <GameRatingProvider>
       <Header />
+      <GameRatingForm />
       <GameRatingList />
-    </>
+    </GameRatingProvider>
 
 
   );

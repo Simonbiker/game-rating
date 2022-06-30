75e2d6d36a589ba5e99332a6ad32120e3feae358
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-06-30 15:16:24 +0100

    Making style changes and static data

---
+++ src/data/GameRatingData.js
@@ -0,0 +1,22 @@
+const GameRatingData = [
+    {
+        id: 1,
+        title: 'Title of game one',
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+        id: 2,
+        title: 'Title of game two',
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+        id: 3,
+        title: 'Title of game three',
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+]
+
+export default GameRatingData
